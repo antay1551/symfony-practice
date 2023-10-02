109fdc664587acb836d1593cf1cc884bdcdbd8c7
Anton Tolok <antay1551@gmail.com>
2023-10-02 12:59:47 +0300

    added pagination

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product_index')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, Request $request): Response
     {
-        $products = $productRepository->findAll();
+        $queryBuilder = $productRepository->createQueryBuilder('product');
+        $adapter = new QueryAdapter($queryBuilder);
+        $products = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            $adapter,
+            $request->query->get('page', 1),
+            9
+        );
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
